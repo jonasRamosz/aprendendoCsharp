135e8755812ad474e3c4b30025e609144fe4c9b5
jonasRamosz <jrenovaveis@gmail.com>
2022-07-25 20:41:26 -0300

    update 1

---
+++ 1-olamundo/Olamundo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+    class Olamundo
+    {   
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá,Mundo!");
+        }
+}
---
+++ 2-criandoVariaveis/criandoVariaveis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Programas
+{
+    static void Main(string[] args)
+    {
+        int n1 = 2;
+        int n2 = 3;
+        int soma = n1 + n2;
+        double salario = 1212.99;
+        double desc = 141.21;
+        double valorFinal = salario - desc;
+        Console.WriteLine(n1 + " + " + n2 + " = " + soma);
+        Console.WriteLine(salario + " - "  + desc + " = " + valorFinal);
+    }
+}
